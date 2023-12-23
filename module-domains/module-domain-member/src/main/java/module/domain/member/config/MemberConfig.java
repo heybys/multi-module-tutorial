22be08055be6497bb326c2f786e23f02d5028f8f
heybys <byn0174@gmail.com>
2023-12-24 02:31:54 +0900

    fix configuration

--- module-domains/module-domain-member/src/main/java/module/domain/member/config/MemberConfig.java
+++ module-domains/module-domain-member/src/main/java/module/domain/member/config/MemberConfig.java
@@ -1,13 +1,13 @@
-package module.domain.member;
+package module.domain.member.config;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@Configuration
 @EntityScan("module.domain.member.persistence")
 @EnableJpaRepositories("module.domain.member.persistence")
 @ComponentScan("module.domain.member")
+@Configuration
 public class MemberConfig {
 }
---
+++ module-features/module-feature-member-management/src/main/java/module/feature/membermanagement/config/MemberManagementConfiguration.java
@@ -0,0 +1,9 @@
+package module.feature.membermanagement.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("module.feature.membermanagement")
+@Configuration
+public class MemberManagementConfiguration {
+}
